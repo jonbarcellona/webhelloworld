9f35ba011d1bce1bb4f6012fe75aa0395123c69e
Jon Barcellona <jon@barcellona.com>
2017-06-30 16:49:34 -0500

    initial code - skeleton

--- webhelloworld.go
+++ webhelloworld.go
@@ -5,16 +5,30 @@ http://localhost:8080
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"go-github/github"
 	"log"
 	"net/http"
+	"oauth2"
+	"os"
 )
 
 func main() {
+	portNumber := flag.Int("port", 8080, "port number to listen on")
+	flag.Parse()
+
+	http.HandleFunc("/gcp", handleGCP)
+	http.HandleFunc("/github", handleGithub)
+
 	http.HandleFunc("/", handle)
-	http.HandleFunc("/_ah/health", healthCheckHandler)
-	log.Print("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/health", healthCheckHandler)
+
+	log.Printf("Go simple web server (pid=%d)", os.Getpid())
+	log.Printf("Listening on port %d", *portNumber)
+	portString := fmt.Sprintf(":%d", *portNumber)
+	log.Fatal(http.ListenAndServe(portString, nil))
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +38,25 @@ func handle(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprint(w, "Hello world!")
 }
-
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "ok")
+	fmt.Fprintf(w, "Environment: PID=%d", os.Getpid())
+}
+
+func handleGCP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "GCP")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: "... your access token ..."},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(nil)
+
+	// list all organizations for user "willnorris"
+	orgs, _, err := client.Organizations.List(ctx, "jonbarcellona", nil)
+}
+
+func handleGithub(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "handleGithub")
 }
